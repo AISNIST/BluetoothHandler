a34bde0ad01e4b3adf671b97a5bdc8f16dde8838
Rushad Antia <nope>
2015-02-07 10:41:57 -0500

    cahllenges added

--- src/com/example/bluetoothhandler/MainActivity.java
+++ src/com/example/bluetoothhandler/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.bluetoothhandler;
 
-import com.example.bluetoothhandler.BluetoothHandler.Robot;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -9,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.example.bluetoothhandler.BluetoothHandler.Robot;
 
 public class MainActivity extends Activity {
 
@@ -21,16 +21,55 @@ public class MainActivity extends Activity {
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		//startActivity(new Intent(getApplicationContext(), ChooseDeviceToConnectTo.class));
 		startActivityForResult(new Intent(getBaseContext(),ChooseDeviceToConnectTo.class),1);
+		addReconnectButton();
 
-		((Button)findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
-			
+		 
+		((Button)findViewById(R.id.c1)).setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
- 
-				robot.moveFrontServo(600);
+			 startActivity(new Intent(getApplicationContext(),Challenge1.class));
 			}
-		} );
+		});
 		
+		((Button)findViewById(R.id.c2)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				 startActivity(new Intent(getApplicationContext(),Challenge2.class));
+			}
+		});
+		
+		((Button)findViewById(R.id.c3)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				 startActivity(new Intent(getApplicationContext(),Challenge3.class));
+			}
+		});
+		
+		((Button)findViewById(R.id.c4)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				 startActivity(new Intent(getApplicationContext(),Challenge4.class));
+			}
+		});
+
 	}
- 
+
+	private void addReconnectButton() {
+		((Button)findViewById(R.id.reconnect)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				BluetoothHandler handler = robot.getBTHandler("guy");
+				handler.dispose();
+				robot = null;
+				startActivity(new Intent(getApplicationContext(), ChooseDeviceToConnectTo.class));
+			}
+		});
+
+	}
+
 }
