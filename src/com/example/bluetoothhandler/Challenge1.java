469db4467900df94a1c3e9a94b575bbf31a73742
Rushad Antia <nope>
2015-02-07 10:52:28 -0500

    added stuff

--- src/com/example/bluetoothhandler/Challenge1.java
+++ src/com/example/bluetoothhandler/Challenge1.java
@@ -2,33 +2,24 @@ package com.example.bluetoothhandler;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.example.bluetoothhandler.BluetoothHandler.Robot;
 
 public class Challenge1 extends Activity {
+
+	private Robot robot;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_challenge1);
+		robot = MainActivity.robot;
+
+		/**WRITE CODE HERE***/
+
+
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.challenge1, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
